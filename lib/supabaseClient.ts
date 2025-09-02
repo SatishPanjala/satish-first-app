d3f7cb1e46bcd41f956b53d6751c3e2904147d73
Satish.Panjala <Satish.Panjala@elysiumanalytics.ai>
2025-09-02 13:57:48 +0530

    added url and key in supaBase Client

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://itbfxityovwtutaeftxa.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0YmZ4aXR5b3Z3dHV0YWVmdHhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0NzYzNzYsImV4cCI6MjA3MjA1MjM3Nn0.QTflDuUQ38hWzburHZ5lqraArlaYTjhBF-qvk5CZF3s"
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
